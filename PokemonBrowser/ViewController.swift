460e6108f760880b4f7d8c3063e3a564b6244502
Matt Ruston <mruston@underarmour.com>
2019-03-09 12:51:16 -0600

    Initial Commit

---
+++ PokemonBrowser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PokemonBrowser
+//
+//  Created by Matthew Ruston on 3/9/19.
+//  Copyright © 2019 Under Armour, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
